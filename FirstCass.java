05d48479bbda2927fdd4362931f56ddcfc9c35fe
nasimqureshi <qureshi.nasim@yahoo.com>
2023-01-05 21:06:57 -0600

    commit

--- FirstCass.java
+++ FirstCass.java
@@ -1,5 +1,11 @@
 class FirstClass {
     public static void main(String args[]){
-        System.out.println("Hello Nasim");
+        System.out.println("*");
+        System.out.println("**");
+        System.out.println("***");
+        System.out.println("****");
+
+        
+
     }
 }
